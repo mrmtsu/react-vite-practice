7dfcabd0c032675c3c6d04c5eeece4f78317f50a
mrmtsu <81202muramatsu@gmail.com>
2023-02-12 13:32:03 +0900

    add about page

--- src/components/container/About.tsx
+++ src/components/container/About.tsx
@@ -1,7 +1,28 @@
+import aboutImage from "../../assets/about.jpg";
+
 const About = () => {
   return (
     <div className="section" id="about">
-      About
+      <div className="grid md:grid-cols-2 gap-8 place-items-center">
+        <div className="border-[3px] border-solid border-Teal rounded-lg">
+          <img src={aboutImage} alt="" className="p-4" />
+        </div>
+        <div>
+          <div className="font-bold sm:text-[1.875rem] text-[1.5rem] mb-5">
+            We provide the <br /> best{" "}
+            <span className="text-Teal">online courses</span>
+          </div>
+          <p className="text-sm text-gray leading-7 mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repudiandae, totam earum. Minus deleniti repellat id! Ratione quia
+            eum, explicabo quos eos magni vel corporis voluptatibus, inventore
+            doloremque aliquam pariatur recusandae.
+          </p>
+          <button className="py-3 px-6 text-sm border border-solid border-gray rounded-lg font-bold">
+            Know More
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
