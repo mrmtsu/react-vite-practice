ab630f681265c863c009d98efa8fea4e61aa29c9
mrmtsu <81202muramatsu@gmail.com>
2023-02-14 07:00:49 +0900

    add contact page

--- src/components/container/Contact.tsx
+++ src/components/container/Contact.tsx
@@ -1,7 +1,34 @@
+import { motion } from "framer-motion";
+
 const Contact = () => {
   return (
     <div className="section" id="contact">
-      Contact
+      <div className="text-center max-w-[600px] mx-auto">
+        <div className="sm:text-3xl text-2xl font-bold mb-5">
+          Subscribe Newsletter
+        </div>
+        <p className="text-sm leading-7 text-gray">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+          perferendis debitis dolorum facilis culpa, quidem volupatate suscipt
+          deserunt. Magni neque at eos dolore dignissimos fugit repudiadae? Aut
+          laundantium asperiores et!
+        </p>
+        <motion.form
+          initial={{ scale: 0 }}
+          whileInView={{ scale: 1 }}
+          transition={{ duration: 0.3 }}
+          className="mt-5"
+        >
+          <input
+            type="text"
+            placeholder="Enter your email address"
+            className="sm:p-3 p-2 outline-none text-sm shadow-md sm:w-72 w-60"
+          />
+          <button className="text-sm text-white bg-Teal sm:p-3 p-2 shadow-md font-bold">
+            Subscribe
+          </button>
+        </motion.form>
+      </div>
     </div>
   );
 };
